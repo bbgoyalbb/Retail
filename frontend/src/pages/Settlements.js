4c7eff99a899b4aa2ac3de9575cbba8958e2c5ca
bbgoyalbb <bbgoyal.bb@gmail.com>
2026-04-23 19:30:32 +0530

    Fix Settlements.js syntax error: missing closing brace on .map() arrow function

--- frontend/src/pages/Settlements.js
+++ frontend/src/pages/Settlements.js
@@ -319,7 +319,7 @@ export default function Settlements() {
                     )}
                   </div>
                   );
-                })
+                })}
               </div>
             </div>
           )}
