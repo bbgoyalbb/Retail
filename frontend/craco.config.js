8ab5cdb973a45ceac8ad2d054540a52dfdd6ad5c
bbgoyalbb <bbgoyal.bb@gmail.com>
2026-05-19 02:22:24 +0530

    fix: add ESLint react-hooks config back to craco

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,6 +2,15 @@
 const path = require("path");
 
 module.exports = {
+  eslint: {
+    configure: {
+      extends: ["plugin:react-hooks/recommended"],
+      rules: {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+      },
+    },
+  },
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
